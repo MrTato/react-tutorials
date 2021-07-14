4712f04a1b4588dd1057ce6dfac49cac64a5bd66
Bayardo <bjlslopez@gmail.com>
2021-07-14 13:10:45 -0600

    25. Rendering a list

--- reactjs-quickstart/src/App.js
+++ reactjs-quickstart/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 // import './App.css';
 import PageNavbar from './components/PageNavbar';
+import ListFacts from './components/ListFacts';
 
 function App() {
   return (
     <div>
       <PageNavbar/>
+      <ListFacts />
     </div>
   );
 }
