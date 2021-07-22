7a5cf2a899fe69a8419677c74b588f8da1dbfed0
Bayardo <bjlslopez@gmail.com>
2021-07-22 13:14:00 -0600

    29. State of a component

---
+++ reactjs-quickstart/src/components/PostsListPage.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import ListPosts from './ListPosts'; 
+import httpService from '../services/HttpService';
+
+class PostsListPage extends Component {
+    state = {
+        posts: null
+    };
+
+    componentDidMount() {
+        const postsUrl = "https://jsonplaceholder.typicode.com/posts";
+        httpService.get(postsUrl).then(posts => this.setState({
+            posts: posts
+        }));
+    }
+
+    render() {
+        const {posts} = this.state;
+
+        const headerStyle = { textAlign: "center" };
+        if (!posts) return <h1 style={headerStyle}>Loading...</h1>
+        return (
+            <div>
+                <h1 className="text-center">Posts (2/{posts.length})</h1>
+                <ListPosts posts={posts} limit={2} />
+            </div>
+        );
+    }
+}
+export default PostsListPage;
